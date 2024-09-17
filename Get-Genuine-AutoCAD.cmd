137430006b232eeb472bf0a2c37834f999fc127c
nguyenhiep2805 <nguyenhiep.190217@gmail.com>
2024-09-17 14:37:28 +0700

    First commit

---
+++ Get-Genuine-AutoCAD.cmd
@@ -0,0 +1,5 @@
+echo 127.0.0.1 genuine-software.autodesk.com >> C:\Windows\System32\drivers\etc\hosts
+echo 127.0.0.1 genuine-software1.autodesk.com >> C:\Windows\System32\drivers\etc\hosts
+echo 127.0.0.1 genuine-software2.autodesk.com >> C:\Windows\System32\drivers\etc\hosts
+echo 127.0.0.1 ase-cnd-stg.autodesk.com >> C:\Windows\System32\drivers\etc\hosts
+echo 127.0.0.1 ase.autodesk.com >> C:\Windows\System32\drivers\etc\hosts
