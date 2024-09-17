8275f511ed19dfdff4909fef45bd8d3f1f7d6269
nguyenhiep2805 <nguyenhiep.190217@gmail.com>
2024-09-17 15:05:23 +0700

    First commit

--- Get-Genuine-AutoCAD.cmd
+++ Get-Genuine-AutoCAD.cmd
@@ -55,7 +55,6 @@ echo 127.0.0.1 genuine-software2.autodesk.com >> C:\Windows\System32\drivers\etc
 echo 127.0.0.1 ase-cnd-stg.autodesk.com >> C:\Windows\System32\drivers\etc\hosts
 echo 127.0.0.1 ase.autodesk.com >> C:\Windows\System32\drivers\etc\hosts
 echo ^> Da sua loi thanh cong...
-
 :MSAct
 cls
 echo:
